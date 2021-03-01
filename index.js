1c926bb715fda158051e62c8c5508f90cb3ac8af
Hanna Munoz <hannamunoz@gmail.com>
2021-02-28 21:13:28 -0500

    testing html generation

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ function formTeam() {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css">
-    <title>${teamArray[0]}<title>
+    <title>${teamArray[0]}</title>
 </head>
     
 
